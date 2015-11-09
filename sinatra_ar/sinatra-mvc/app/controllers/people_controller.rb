2553f85e69f2c7414e3e551086d5bfd0dad83ee8
Sadiamalik1980 <sadiamalik1980@gmail.com>
2015-11-09 01:28:11 -0500

    CRUD_people

--- sinatra_ar/sinatra-mvc/app/controllers/people_controller.rb
+++ sinatra_ar/sinatra-mvc/app/controllers/people_controller.rb
@@ -3,10 +3,44 @@ get '/people/' do
   erb :"/people/index"
 end
 
+get '/people/new' do
+    erb :"/people/new"
+end 
+
+post '/people' do
+  if params[:birthdate].include?("-")
+    birthdate = params[:birthdate]
+  else
+    birthdate = Date.strptime(params[:birthdate], "%m%d%Y")
+  end
+  
+  person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
+  redirect "/people/#{person.id}"
+end
+
+get '/people/:id/edit' do
+    @person = Person.find(params[:id])
+    erb :'/people/edit'
+end
+
+put '/people/:id' do
+    person = Person.find(params[:id])
+    person.first_name = params[:first_name]
+    person.last_name = params[:last_name]
+    person.birthdate = params[:birthdate]
+    person.save
+    redirect "/people/#{person.id}"    
+  end
+
+delete '/people/:id' do
+  person = Person.find(params[:id])
+  person.delete
+  redirect "/people"
+end
+
 get '/people/:id' do
     @person = Person.find(params[:id])
-    birthdate_string = @person.birthdate.strftime("%m%d%Y")
-    @birth_path_num = Person.dob_pathnumber(birthdate_string)
+    @birth_path_num = Person.dob_pathnumber(@person.birthdate.strftime("%m%d%Y"))
     @message = Person.dob_message(@birth_path_num)  
    erb :"/people/show"
 end
