5f09ba919178c4c4a0f0c5dca612f81bcd3d78a2
Sadiamalik1980 <sadiamalik1980@gmail.com>
2015-11-30 02:44:56 -0500

    Anagrams

---
+++ anagrams/anagram.rb
@@ -0,0 +1,16 @@
+require_relative 'reverse_letters'
+
+def find_anagram(letters)
+   length = letters.length
+   letters = letters.split("")
+
+   anagrams = []
+
+   letters.each_with_index do |letter, index|
+     remaining_letters = letters.select {|l| l != letter}
+     anagrams << letter + remaining_letters.join
+
+     anagrams << letter + reverse_letters(remaining_letters).join
+   end
+   anagrams
+ end
